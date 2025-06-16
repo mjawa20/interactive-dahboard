8bfb3bfaeb9f334631f26550380d86a6ffa92ca5
m.jawa <m.jawahiruzzaman@gmail.com>
2025-06-16 08:42:12 +0700

    add pointer

--- src/components/organisms/Navbar.tsx
+++ src/components/organisms/Navbar.tsx
@@ -5,7 +5,7 @@ export default function Navbar() {
   const { activePage, setIsShowMobileSidebar, isShowMobileSidebar } = useGlobalStore();
   return (
     <div className='flex items-center gap-3 bg-white p-3 shadow'>
-      <button className='bg-white p-2 ' onClick={() => { setIsShowMobileSidebar(!isShowMobileSidebar) }}>
+      <button className='bg-white p-2 cursor-pointer' onClick={() => { setIsShowMobileSidebar(!isShowMobileSidebar) }}>
         <Menu className='h-5 w-5' />
       </button>
       <h1 className="text-xl font-semibold">{activePage?.name}</h1>
